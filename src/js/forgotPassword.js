57e827540451ee6d97c77baea59a3ce76ab2db1a
Megan <burn8828@kettering.edu>
2024-06-06 21:24:27 -0400

    styled more of the website

--- src/js/forgotPassword.js
+++ src/js/forgotPassword.js
@@ -1,14 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-analytics.js';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
-// Function to handle the "Back to Login" button click event
-function backToLogin() {
-    // Redirect to the login page
-    window.location.href = 'login.html';
-}
 
-// Add event listener to the "Back to Login" button
-document.getElementById('back-button').addEventListener('click', backToLogin);
 
 
 // Function to handle the form submission for password reset
